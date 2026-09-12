fb1444922a90fb9e64a721eba6f854561891912a
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2026-09-12 16:28:28 +0200

    kk

---
+++ pazw/cw2/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.WriteLine("Hello, World!");
+
+
+void ShowVertical(string text)
+{
+    for (int i=0; i<text.Length ;i++ ) {
+        Console.WriteLine(text[i]);
+    }
+    //for(; ; )
+    //{
+    //    Console.WriteLine("ii");
+    //}
+}
+void ShowReverse(string text)
+{
+    for (int i = text.Length - 1; i >= 0; i--) { 
+        Console.Write(text[i]);
+    }
+}
+void ShowRandomSum(uint max)
+{
+    uint sum = 0;
+    var rnd = new Random();
+    while (sum < max)
+    {
+        uint random = (uint)rnd.Next(20);
+        Console.Write(random + " ");
+        sum += random;
+    }
+    Console.WriteLine($"suma wynosi: {sum}");
+}
+void RandomUntilZero()
+{
+    var rnd = new Random();
+    //losowanie do momentu zera i wyswietlenie
+}
+
+ShowVertical("Ala ma kota");
+ShowReverse("ala ma kota");
+Console.WriteLine("\n=================================\n");
+ShowRandomSum(200);
