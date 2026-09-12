443c03e20f7c46fcf6b78ad66a93842fbc6d11de
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2026-09-12 12:38:25 +0200

    Update cw1.kt

--- pam/cw1/app/src/main/java/com/example/cw1/cw1.kt
+++ pam/cw1/app/src/main/java/com/example/cw1/cw1.kt
@@ -1,12 +1,19 @@
 package com.example.cw1
 
 fun main(){
-    val firstname:String = "Jan";
-    val lastname:String = "Kowakski";
-    val age:Int = 43;
-    println(Hello(firstname,lastname,age));
-}
-fun Hello(firstname:String,lastname:String,age:Int):String {
+//    val firstname:String = "Jan";
+//    val lastname:String = "Kowakski";
+    print("Podaj imie: ")
+    var firstname =  readLine() ?: ""
+    print("Podaj nazwisko: ")
+    var lastname = readlnOrNull() ?: ""
+    print("Podaj wiek: ")
+    var age = readLine()?.toIntOrNull() ?: 0
+//    var age:Int = if (agge?.length!=0) agge.toInt() else 0
 
+    println(Hello(firstname,lastname,age));
+    println(Hello(firstname,lastname));
+}
+fun Hello(firstname:String,lastname:String,age:Int=67):String {
     return "Witaj: "+firstname+" "+lastname+ " wiek: "+age
 }
