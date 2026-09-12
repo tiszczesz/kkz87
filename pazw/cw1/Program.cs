118cf63abf727662c42c9d6d17a2182c948722c2
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2026-09-12 15:40:07 +0200

    kk

--- pazw/cw1/Program.cs
+++ pazw/cw1/Program.cs
@@ -3,17 +3,25 @@
 //  komunikat o błędzie
 void RegisterUser()
 {
-    var bb = Char.IsUpper('a');
-    string email = "asdad@sdfsdfsdf";
-    email.Contains("@");
+    Console.WriteLine("Podaj swój email: ");
+    string? inputEmail = Console.ReadLine();
+     Console.WriteLine("Podaj swoje hasło: ");
+    string? password = Console.ReadLine();
+    // if (ValidateEmail(email)){
+    //     Console.WriteLine("Email jest poprawny.");
+    // }
+    Console.WriteLine(ValidateEmail(inputEmail) ? "Email jest poprawny." : "Email jest niepoprawny.");
+    Console.WriteLine(ValidatePassword(password) ? "Hasło jest poprawne." : "Hasło jest niepoprawne.");
+
 }
 
-void fun1()
-{  
+static void GreetUser()
+{
     Console.WriteLine("Hello World");
     string napis = "Hello World";
     Console.WriteLine(napis);
-    try{
+    try
+    {
         Console.WriteLine("Please enter your first name:");
         string? firstname = Console.ReadLine();
         Console.WriteLine("Please enter your birth year:");
@@ -25,6 +33,26 @@ void fun1()
     {
         Console.WriteLine($"An error occurred: {ex.Message}");
     }
-    
+}
+
+bool ValidatePassword(string? password)
+{
+    // Prosta walidacja hasła
+    return password != null && password.Length >= 8 && password.Any(char.IsUpper) 
+        && password.Any(char.IsLower) && password.Any(char.IsDigit); 
+}
+bool ValidateEmail(string? email)
+{
+    // Prosta walidacja emaila
+    return email != null && email.Length > 4 && email.Contains("@") && email.Contains(".");   
+}
+
+
+void fun1()
+{
+   // GreetUser();
+    Console.WriteLine(" ======================================================== \n\n");
+    RegisterUser();
+
 }
 fun1();
